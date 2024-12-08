d0ef535c83e074eeb9c8a3f4fd1efb786990a1a6
abhi <abhikirk@icloud.com>
2024-12-08 08:06:48 -0800

    day8 optimized with complexity added

--- day8.py
+++ day8.py
@@ -1,5 +1,8 @@
 # Resonant Collinearity
 
+# f := number of frequencies
+# k := number of antennas per freq
+
 import pathlib
 import os
 from collections import defaultdict
@@ -18,96 +21,90 @@ chars = [chr(ch + 97) for ch in range(26)]
 chars += [ch.upper() for ch in chars]
 ants = ints + chars
 
-# Part 1
 antennas = defaultdict(list)
 
 m, n = len(data), len(data[0])
-for row in range(m):
+for row in range(m):  # O(m.n)
     for col in range(n):
         if data[row][col] in ants:
             antennas[data[row][col]].append((row, col))
 
 
-def get_combinations(arr):
-    res = []
-
-    def backtrack(start, path):
-        if len(path) == 2:
-            res.append(path[:])
-
-        for i in range(start, len(arr)):
-            node = arr[i]
-            if node in path:
-                continue
-            path.append(node)
-            backtrack(i, path)
-            path.pop()
-
-    backtrack(0, [])
-    return res
+# def get_combinations(arr):
+#     res = []
+#
+#     def backtrack(start, path):
+#         if len(path) == 2:
+#             res.append(path[:])
+#
+#         for i in range(start, len(arr)):
+#             node = arr[i]
+#             if node in path:
+#                 continue
+#             path.append(node)
+#             backtrack(i, path)
+#             path.pop()
+#
+#     backtrack(0, [])
+#     return res
 
 def is_valid(i, j):
     if 0 <= i < m and 0 <= j < n:
         return True
     return False
 
-def get_antinodes(n1, n2, get_all=False):
+def get_collinear_points(points, get_all=False):
     res = set()
 
-    row_diff, col_diff = abs(n1[0] - n2[0]), abs(n1[1] - n2[1])
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            x1, y1 = points[i]
+            x2, y2 = points[j]
 
-    x1, x2 = min(n1[0], n2[0]) - row_diff, max(n1[0], n2[0]) + row_diff
-    if n1[1] >= n2[1]:
-        y1, y2 = max(n1[1], n2[1]) + col_diff, min(n1[1], n2[1]) - col_diff
-    else:
-        y1, y2 = min(n1[1], n2[1]) - col_diff, max(n1[1], n2[1]) + col_diff
+            if get_all:  # add self
+                res.add((x1, y1))
+                res.add((x2, y2))
 
-    if is_valid(x1, y1):
-        res.add((x1, y1))
-    if is_valid(x2, y2):
-        res.add((x2, y2))
+            # get direction vector
+            dy, dx = y2 - y1, x2 - x1
 
-    if get_all:
-        res.add(n1)
-        res.add(n2)
+            # normalize to get smallest step in direction
+            divisor = math.gcd(abs(dy), abs(dx))
+            dy //= divisor
+            dx //= divisor
 
-        row_step = n2[0] - n1[0]
-        col_step = n2[1] - n1[1]
-        step = math.gcd(row_diff, col_diff)
+            # find collinear points: backward
+            x, y = x1 - dx, y1 - dy
+            if is_valid(x, y):
+                res.add((x, y))
+            if get_all:
+                while is_valid(x, y):
+                    res.add((x, y))
+                    x -= dx
+                    y -= dy
 
-        row_step //= step
-        col_step //= step
+            # find collinear points: forward
+            x, y = x2 + dx, y2 + dy
+            if is_valid(x, y):
+                res.add((x, y))
+            if get_all:
+                while is_valid(x, y):
+                    res.add((x, y))
+                    x += dx
+                    y += dy
 
-        x, y = n1[0] - row_step, n1[1] - col_step
-        while is_valid(x, y):
-            res.add((x, y))
-            x -= row_step
-            y -= col_step
-
-        x, y = n2[0] + row_step, n2[1] + col_step
-        while is_valid(x, y):
-            res.add((x, y))
-            x += row_step
-            y += col_step
     return res
 
 
+# Part 1: O(m.n) + O(f.k^2)
 antinodes_all = set()
-for ant in antennas:
-    locs_combinations = get_combinations(antennas[ant])
-    for node1, node2 in locs_combinations:
-        antinodes = get_antinodes(node1, node2)
-        antinodes_all = antinodes_all.union(antinodes)
-
+for freq, freq_antennas in antennas.items():  # O(f)
+    antinodes_all.update(get_collinear_points(freq_antennas))  # O(k^2): number of pairs
 print("Number of antinode locations:", len(antinodes_all))
 
 
-# Part 2
+# Part 2: O(m.n) + O(f.max(m,n).k^2)
 antinodes_all = set()
-for ant in antennas:
-    locs_combinations = get_combinations(antennas[ant])
-    for node1, node2 in locs_combinations:
-        antinodes = get_antinodes(node1, node2, get_all=True)
-        antinodes_all = antinodes_all.union(antinodes)
-
+for freq, freq_antennas in antennas.items():  # O(f)
+    antinodes_all.update(get_collinear_points(freq_antennas, get_all=True))  # O(max(m, n) . k^2)
 print("Number of updated antinode locations:", len(antinodes_all))
